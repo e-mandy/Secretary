cc79c8d33162cd6caa1a8bdb60d1bed84c0f281b
e-mandy <andymfrd02@gmail.com>
2025-09-05 14:52:04 +0100

    Mise en place des 3 points d'actions

---
+++ public/assets/scripts/script.js
@@ -0,0 +1,6 @@
+const voirButton = document.querySelector('.voirButton')
+const optionsPopup = document.querySelector('.optionsPopup')
+
+voirButton.addEventListener('click', ()=>{
+    optionsPopup.classList.toggle('hidden')
+})
