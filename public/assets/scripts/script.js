d209432f425b8c3817564b44c0f552321b458efe
e-mandy <andymfrd02@gmail.com>
2025-09-10 17:04:57 +0100

    Correction de l'erreur d'affichage d'une popup unitaire

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,11 +1,9 @@
 const voirButtons = document.querySelectorAll('.voirButton')
-console.log(voirButtons)
-const optionsPopup = document.querySelectorAll('.optionsPopup')
+
 
 voirButtons.forEach(voirButton => {
     voirButton.addEventListener('click', ()=>{
-        optionsPopup.forEach(optionPopup =>{
-            optionPopup.classList.toggle('hidden')
-        })
+        const popup = voirButton.querySelector('.optionsPopup')
+        popup.classList.toggle('hidden')
     })
 })
