70d3da0ca47fedffe6d7c15aa105c57459f2927a
e-mandy <andymfrd02@gmail.com>
2025-09-10 16:51:25 +0100

    Correction de l'erreur de popup affichage for table

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,6 +1,11 @@
-const voirButton = document.querySelector('.voirButton')
-const optionsPopup = document.querySelector('.optionsPopup')
+const voirButtons = document.querySelectorAll('.voirButton')
+console.log(voirButtons)
+const optionsPopup = document.querySelectorAll('.optionsPopup')
 
-voirButton.addEventListener('click', ()=>{
-    optionsPopup.classList.toggle('hidden')
+voirButtons.forEach(voirButton => {
+    voirButton.addEventListener('click', ()=>{
+        optionsPopup.forEach(optionPopup =>{
+            optionPopup.classList.toggle('hidden')
+        })
+    })
 })
--- resources/views/professeur/index.blade.php
+++ resources/views/professeur/index.blade.php
@@ -192,7 +192,7 @@
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
                                             </svg>
-                                            <div class="optionsPopup bg-white border border-gray-300 p-2 w-[130px] rounded absolute top-[100%] left-5 hidden">
+                                            <div class="optionsPopup bg-white border border-gray-300 p-2 w-[130px] rounded absolute top-[100%] left-0 hidden">
                                                 <ul class="flex flex-col gap-2">
                                                     <li>
                                                         <a class="flex gap-3 items-center hover:bg-gray-200 p-1 rounded" href="{{ route('admin.professeurs.show', $professeur) }}">
@@ -204,7 +204,7 @@
                                                         </a>
                                                     </li>
                                                     <li>
-                                                        <a class="flex gap-3 items-center hover:bg-gray-200 p-1 rounded" href="#">
+                                                        <a class="flex gap-3 items-center hover:bg-gray-200 p-1 rounded" href="{{ route('admin.professeurs.edit', $professeur) }}">
                                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                                             </svg>
