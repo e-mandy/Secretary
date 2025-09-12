cba031fc4be84b9910b250af500454424536fb9e
e-mandy <andymfrd02@gmail.com>
2025-09-12 14:51:41 +0100

    Correction de l'angle d'affichage de la popup

--- resources/views/document/index.blade.php
+++ resources/views/document/index.blade.php
@@ -76,7 +76,7 @@
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
                                 </svg>
-                                <div class="optionsPopup bg-white border border-gray-300 p-2 w-[130px] rounded absolute top-[100%] left-5 hidden">
+                                <div class="optionsPopup bg-white border border-gray-300 p-2 w-[130px] rounded absolute top-[100%] right-0 hidden">
                                     <ul class="flex flex-col gap-2">
                                         <li>
                                             <a class="flex gap-3 items-center hover:bg-gray-200 p-1 rounded" href="{{ route('admin.documents.edit', $document) }}">
