89932b761b3b457e93b6f0e83016ffe6b4c13a30
e-mandy <andymfrd02@gmail.com>
2025-10-01 10:26:15 +0100

    Refactoring register interface with input component

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,20 +7,14 @@
     <h1 class="text-center text-2xl font-bold mb-4">Connectez-vous à votre compte</h1>
     <form action={{ route('admin.login.store') }} method="POST" class="w-full flex flex-col gap-5">
         @csrf
-        <fieldset class="rounded-lg pt-0 px-[12px] pb-[12px] w-[90%] border border-black m-auto">
-            <legend class="text-[0.85rem]">Email</legend>
-            <input class="border-none w-full outline-none placeholder:text-[0.8rem]" type="email" name="email" placeholder="Ex: d'ALMEIDA">
-            @error('email')
-                <p class="text-red-500 text-sm">{{ $message }}</p>
-            @enderror
-        </fieldset>
-        <fieldset class="rounded-lg pt-0 px-[12px] pb-[12px] w-[90%] border border-black m-auto">
-            <legend class="text-[0.85rem]">Mot de passe</legend>
-            <input class="border-none w-full outline-none placeholder:text-[0.8rem]" type="password" name="password" placeholder="Veuillez entrer votre mot de passe">
-            @error('password')
-                <p class="text-red-500 text-sm">{{ $message }}</p>
-            @enderror
-        </fieldset>
+        <x-auth.input type="email" name="email" placeholder="azap@gmail.com">
+            <x-slot:value>Email</x-slot>
+        </x-auth.input>
+
+        <x-auth.input type="password" name="password" placeholder="Veuillez entrer votre mot de passe">
+            <x-slot:value>Mot de passe</x-slot>
+        </x-auth.input>
+
         <button class="w-9/10 rounded-lg border-none block mt-[20px] mx-auto mb-[10px] bg-black h-[50px] text-white cursor-pointer" type="submit">Se connecter</button>
     </form>
 
