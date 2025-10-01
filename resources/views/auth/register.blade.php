98200ce30c313bba1594b32aea97cfc248acb287
e-mandy <andymfrd02@gmail.com>
2025-10-01 10:19:48 +0100

    Fix error from register refactoring to component interface

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,25 +8,25 @@
     <form class="w-full flex flex-col gap-5" action="{{ route('admin.register.store') }}" method="POST">
 
         @csrf
-        <x-input type="text" name="lastname" placeholder="Ex: d'ALMEIDA">
-            <x-input:value>Nom</x-input:value>
-        </x-input>
+        <x-auth.input type="text" name="lastname" placeholder="Ex: d'ALMEIDA">
+            <x-slot:value>Nom</x-slot>
+        </x-auth.input>
 
-        <x-input type="text" name="firstname" placeholder="Ex: d'ALMEIDA">
-            <x-input:value>Prénom</x-input:value>
-        </x-input>
+        <x-auth.input type="text" name="firstname" placeholder="Ex: d'ALMEIDA">
+            <x-slot:value>Prénom</x-slot>
+        </x-auth.input>
 
-        <x-input type="email" name="email" placeholder="Veuillez entrer votre email">
-            <x-input:value>Email</x-input:value>
-        </x-input>
+        <x-auth.input type="email" name="email" placeholder="Veuillez entrer votre email">
+            <x-slot:value>Email</x-slot>
+        </x-auth.input>
 
-        <x-input type="password" name="password" name="password" placeholder="Veuillez entrer votre mot de passe">
-            <x-input:value>Mot de passe</x-input:value>
-        </x-input>
+        <x-auth.input type="password" name="password" name="password" placeholder="Veuillez entrer votre mot de passe">
+            <x-slot:value>Mot de passe</x-slot>
+        </x-auth.input>
 
-        <x-input type="password" name="password_confirmation" name="password" placeholder="Veuillez entrer votre mot de passe">
-            <x-input:value>Mot de passe confirmé</x-input:value>
-        </x-input>
+        <x-auth.input type="password" name="password_confirmation" name="password" placeholder="Veuillez entrer votre mot de passe">
+            <x-slot:value>Mot de passe confirmé</x-slot>
+        </x-auth.input>
 
         <button class="w-9/10 rounded-lg border-none block mt-[20px] mx-auto mb-[10px] bg-black h-[50px] text-white cursor-pointer" type="submit">S'inscrire</button>
     </form>
--- resources/views/components/auth/input.blade.php
+++ resources/views/components/auth/input.blade.php
@@ -1,7 +1,7 @@
 <fieldset class="rounded-lg pt-0 px-[12px] pb-[12px] w-[90%] border border-black m-auto">
     <legend class="text-[0.85rem]">{{ $value }}</legend>
     <input class="border-none w-full outline-none placeholder:text-[0.8rem]" type="{{ $type }}" name="{{ $name }}" placeholder="{{ $placeholder }}">
-    @error({{ $name }})
+    @error("{{ $name }}")
         <p class="text-red-500 text-sm">{{ $message }}</p>
     @enderror
 </fieldset>
